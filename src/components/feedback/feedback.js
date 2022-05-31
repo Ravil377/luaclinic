d9f4526feccfd87a3a36e3ab78aaf95de1fffb82
Ravil Shamilov <75186385+Ravil377@users.noreply.github.com>
2022-05-31 18:47:57 +0500

    nav slider

--- src/components/feedback/feedback.js
+++ src/components/feedback/feedback.js
@@ -1,17 +1,26 @@
 import Swiper, { Navigation } from 'swiper';
+Swiper.use([Navigation]);
 
-const swiper = new Swiper('.feedback__slider', {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    loop: true,
-    breakpoints: {
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 20  
-        },
-        1200: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        }
-      }
-});
+const swiperFeedback = '.feedback__slider';
+
+if (document.querySelector(swiperFeedback)) {
+  const feedbackSwiper = new Swiper(swiperFeedback, {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      loop: true,
+      breakpoints: {
+          768: {
+              slidesPerView: 2,
+              spaceBetween: 20  
+          },
+          1200: {
+            slidesPerView: 3,
+            spaceBetween: 30
+          }
+      },
+      navigation: {
+        nextEl: '.slider-nav-feedback__next-btn-js',
+        prevEl: '.slider-nav-feedback__prev-btn-js',
+      },
+  });
+}
