f83dcf124b12d43e2f2b4f91101d36e01710017d
Ravil Shamilov <75186385+Ravil377@users.noreply.github.com>
2022-06-03 00:12:02 +0500

    add footer

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -4,14 +4,18 @@ menu && menu.addEventListener('click', (e) => {
     const isLi = e.target.classList.contains('menu__item-title');
     
     if(isLi) {
-        const ulSubMenu = menu.querySelectorAll('.menu__list-subtitle');
-        ulSubMenu.forEach(item => {
-            if(!item.classList.contains('d-none')) {
-                item.classList.add('d-none');
-            }
-        })
-        const ul = e.target.querySelector('ul');
-        ul.classList.remove('d-none');
+        const ulList = e.target.querySelector('ul');
+        if(!ulList.classList.contains('d-none')) {
+            ulList.classList.add('d-none');    
+        } else {
+            const ulSubMenu = menu.querySelectorAll('.menu__list-subtitle');
+            ulSubMenu.forEach(item => {
+                if(!item.classList.contains('d-none')) {
+                    item.classList.add('d-none');
+                }
+            })
+            ulList.classList.remove('d-none');
+        }        
     }
     
 })
